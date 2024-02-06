16e341892806a0af0b032412c7cf072993abcbd7
Ian Irving <ian@falsepositives.com>
2024-02-06 15:36:42 -0500

    Add CartStore and implement addItem method

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-
 import { useProductStore } from "@/stores/ProductStore";
+import { useCartStore } from "@/stores/CartStore";
+
 const productStore = useProductStore();
+const cartStore = useCartStore();
+
 productStore.fetchProducts();
+
 </script>
 
 <template>
   <div class="container">
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
-      <ProductCard v-for="product in productStore.products" :key="product.name" :product="product" />
+      <ProductCard v-for="product in productStore.products" :key="product.name" :product="product"
+        @addToCart="cartStore.addItem($event, product)" />
     </ul>
   </div>
 </template>
---
+++ src/stores/CartStore.js
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+
+export const useCartStore = defineStore("CartStore", {
+  // state
+  state: () => {
+    return {
+      items: [],
+    };
+  },
+  // actions
+  actions: {
+    addItem(count, item) {
+      count = parseInt(count);
+      if (isNaN(count) || count <= 0) return;
+      this.$patch((state) => {
+        for (let index = 0; index < count; index++) {
+          state.items.push({ ...item }); // clone the item so its reference is not shared
+        }
+      });
+    },
+
+    // alternative way to add item to cart without patch
+    // count = parseInt(count);
+    // for (let index = 0; index < count; index++) {
+    //   this.items.push({...item});
+    // }
+  },
+  // getters
+});
