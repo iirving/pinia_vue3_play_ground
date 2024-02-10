407c887ab626d24583e2c57ae13744167e3fe77e
Ian Irving <ian@falsepositives.com>
2024-02-09 23:57:57 -0500

    Refactor CartItem.vue to use display currency function

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,4 +1,5 @@
 <script setup>
+import useDisplayCurrency from "../composables/useDisplayCurrency.js";
 defineProps({
   product: { type: Object, required: true },
   count: { type: Number, required: true },
@@ -12,7 +13,7 @@ defineEmits(["updateCount", "clear"]);
     <span>{{ product.name }}</span>
     <span class="whitespace-nowrap">
       <AppCountInput :model-value="count" @update:modelValue="$emit('updateCount', $event)" />
-      <span class="inline-block w-12 text-right">${{ count * product.price }}</span>
+      <span class="inline-block w-12 text-right">{{ useDisplayCurrency(count * product.price) }}</span>
       <span class="ml-4" @click="$emit('clear')">
         <fa icon="trash-alt" />
       </span>
