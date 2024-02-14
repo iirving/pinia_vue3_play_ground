574e6f8ac127b772a3bdb87b3f9ff3757bceea25
Ian Irving <ian@falsepositives.com>
2024-02-14 18:20:11 -0500

    Add user name to header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,21 @@
 <script setup>
 // imports
+import { computed } from "vue";
 import CartWidget from './CartWidget.vue';
+import { useAuthUserStore } from "../stores/AuthUserStore.js";
+
+const authUserStore = useAuthUserStore();
+const userName = computed(() => authUserStore.userName)
+
 </script>
 
 <template>
   <header class="flex justify-between p-6 mb-10 items-center"
     style="background-image: url('/images/double-bubble-outline.png')">
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget />
+    <div>
+      <span class="mr-2">{{ userName }}</span>
+      <CartWidget class="inline-block" />
+    </div>
   </header>
 </template>
