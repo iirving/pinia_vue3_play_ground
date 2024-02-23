98f9e50c23852e54221eaef81f95190993dcefbb
Ian Irving <ian@falsepositives.com>
2024-02-23 12:05:23 -0500

    Refactor useDisplayCurrency to use getCurrencySymbol function

--- src/composables/useDisplayCurrency.js
+++ src/composables/useDisplayCurrency.js
@@ -1,4 +1,11 @@
 export default function useDisplayCurrency(amount) {
-  const currencySymbol = "$";
-  return `${currencySymbol}${amount}`;
+  const getCurrencySymbol = () => {
+    // This is a mock function that returns the currency symbol
+    // based on the user's locale, or the user's preference,
+    // else fallbacks to the default currency symbo
+    const defaultCurrencySymbol = "$";
+    return defaultCurrencySymbol;
+  };
+
+  return `${getCurrencySymbol()}${amount}`;
 }
